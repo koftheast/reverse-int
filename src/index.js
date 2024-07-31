243d6adf8073e77f3ecd3f3cb3f1c6585b17e4e4
Ruzanna <ruzanna30a@gmail.com>
2024-07-31 21:20:00 +0400

    init: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    const str = n.toString();
+    const reversedStr = str.split('').reverse().join('');
+    const reversedNum = parseInt(reversedStr);
+
+    return reversedNum;
 }
